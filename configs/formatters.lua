95162426b5c59c17b26490758e16154b73bbc255
shooterfei <shooterfei@163.com>
2022-04-15 17:24:48 +0800

    新增markdown 格式化配置

--- configs/formatters.lua
+++ configs/formatters.lua
@@ -3,6 +3,7 @@ local formatters = require("lvim.lsp.null-ls.formatters")
 formatters.setup({
 	{ command = "black", filetypes = { "python" } },
 	{ command = "isort", filetypes = { "python" } },
+	{ command = "prettierd", filetypes = { "markdown" } },
 	{
 		-- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
 		command = "prettier",
